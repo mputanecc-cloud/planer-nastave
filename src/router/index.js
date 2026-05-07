3d3a894269a7104922b45022af9c4fe1bc1e0228
Magdalena Putanec Cetinić <mputanecc@student.unipu.hr>
2026-05-07 14:59:51 +0200

    Ispravak routera

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,6 @@ import Vue from "vue"
 import VueRouter from "vue-router"
 import LoginPage from "../views/LoginPage.vue"
 import ProfilPage from "../views/ProfilPage.vue"
-import ListiciPage from "../views/ListiciPage.vue"
-import FavoritiPage from "../views/FavoritiPage.vue"
 
 Vue.use(VueRouter)
 
